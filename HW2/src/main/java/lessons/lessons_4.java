51403ddcb4c7b296d45cbba337a12150c4345061
Aleksandr Vasilyevykh <myvps45@gmail.com>
2022-10-18 23:08:24 +0600

    Add new lessons, add git ignore

---
+++ HW2/src/main/java/lessons/lessons_4.java
@@ -0,0 +1,64 @@
+package lessons;
+
+import java.util.Scanner;
+
+public class lessons_4 {
+    public static void main(String[] args) {
+//        for (int i = 1; i <= 10; i++ ){
+//            for (int n = 1; n <= 10; n++){
+//                int j;
+//                j = i * n;
+//                if (i < n) {
+//                    System.out.println("Equals");
+//                    break;
+//                }
+//                System.out.println(i + " * " + n + " = " + j);
+//            }
+//            System.out.println(String.format("%10s", "").replace(' ', '*'));
+//        }
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number 1: ");
+        int n = sc.nextInt();
+        System.out.println("Enter number 2: ");
+        int m = sc.nextInt();
+
+        if (!(m > n)){
+            System.out.println("Error");
+            return;
+        }
+
+//        int numberNine = 0;
+//
+//        for (int i = n; i <= m; i++) {
+//            int number = i;
+//            while (number > 0){
+//                int fig = number%10;
+//                if (fig == 9){
+//                    numberNine ++;
+//                }
+//                number/=10;
+//            }
+//        }
+//        System.out.println("nine number = " + numberNine);
+//        System.out.println("Ok");
+
+        int maxSumFigure = 0;
+        int maxNumber = 0;
+        for (int i = n; i <= m; i++){
+            int number = i;
+            int sumFigure = 0;
+            while (number > 0){
+                int fig = number%10;
+                sumFigure += fig;
+                number /= 10;
+            }
+            if (sumFigure > maxSumFigure){
+                maxSumFigure = sumFigure;
+                maxNumber = i;
+            }
+        }
+        System.out.println(maxNumber);
+
+    }
+}
